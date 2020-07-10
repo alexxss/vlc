2b33280f7c90a220674a5859f048168bf7b636b8
alex-ubuntu <rainbowpulp@gmail.com>
2020-07-11 04:41:11 +0800

    send n recv ok :smiley:

--- scratch/thesis/thesis.cc
+++ scratch/thesis/thesis.cc
@@ -97,6 +97,11 @@ static void CwndTracer(uint32_t oldval, uint32_t newval) {
 	NS_LOG_INFO("Moving cwnd from " << oldval << " to " << newval);
 }
 
+std::string makeName(const std::string& devType, const int& APid, const int& UEid){
+    std::string DeviceName = devType + "-" + std::to_string(APid) + "-" + std::to_string(UEid);
+    return DeviceName;
+}
+
 /*          init node static member       */
 int node::UE_number = 0;
 node* node::transmitter[g_AP_number] = {0};
@@ -107,7 +112,7 @@ int main(int argc, char *argv[]) {
 	// Users may find it convenient to turn on explicit debugging
 	// for selected modules; the below lines suggest how to do this
 	//  LogComponentEnable("TcpSocketImpl", LOG_LEVEL_ALL);
-	//  LogComponentEnable("PacketSink", LOG_LEVEL_ALL);            // uncomment in original example
+//	  LogComponentEnable("PacketSink", LOG_LEVEL_ALL);            // uncomment in original example
 	//  LogComponentEnable("TcpLargeTransfer", LOG_LEVEL_ALL);
 
     //parameters:
@@ -123,7 +128,7 @@ int main(int argc, char *argv[]) {
 		data[i] = m;
 	}
 
-  	for (node::UE_number = 10; node::UE_number<=g_UE_max; node::UE_number+=10) {
+  	for (node::UE_number = 20; node::UE_number<=g_UE_max; node::UE_number+=10) {
 
         algorithm Algorithm;
         Algorithm.fullAlgorithm();
@@ -145,25 +150,6 @@ int main(int argc, char *argv[]) {
             m_listPosition->Add(Vector(x,y,g_AP_height));
 		}
 
-        /*--- read from .dat version ---*/
-        /*std::ifstream fin("../dat/AP_point.dat");
-        if(fin.is_open()){
-            int cnt=0;
-            while(cnt<g_AP_number-1 && fin){
-                double x,y;
-                fin>>cnt>>x>>y;
-                m_listPosition->Add(Vector(x,y, g_AP_height));
-            }
-            if (cnt!=g_AP_number-1) {
-                std::cout<<"AP location not enough? Should get "<<g_AP_number-1<<", got "<<cnt<<" instead";
-                exit(1);
-            }
-            fin.close();
-        } else {
-            std::cout<<"CANT OPEN AP_POINT FILE";
-            exit(1);
-        }*/
-
         ///////////// location for UE ///////////////
         for(node* n : node::receiver){
             if (!n) break;
@@ -171,25 +157,6 @@ int main(int argc, char *argv[]) {
             double y = n->location.second;
             m_listPosition->Add(Vector(x,y,g_UE_height));
         }
-        /*--- read from dat version ---*/
-        /*std::ifstream fin("../dat/UE_point.dat");
-        fin.open("../dat/UE_point.dat");
-        if(fin.is_open()){
-            int cnt=0;
-            while(cnt<node::UE_number-1 && fin){
-                double x,y;
-                fin>>cnt>>x>>y;
-                m_listPosition->Add(Vector(x,y, g_UE_height));
-            }
-            if (cnt!=node::UE_number-1) {
-                std::cout<<"UE location not enough? Should get "<<node::UE_number-1<<", got "<<cnt<<" instead";
-                exit(1);
-            }
-            fin.close();
-        } else {
-            std::cout<<"CANT OPEN UE_POINT FILE";
-            exit(1);
-        }*/
 
         MobilityHelper mobility;
 		mobility.SetPositionAllocator(m_listPosition);
@@ -198,26 +165,139 @@ int main(int argc, char *argv[]) {
 		mobility.Install(allUE);
 
 		VlcDeviceHelper devHelperVPPM; //VLC Device Helper to manage the VLC Device and device properties.
+        VlcChannelHelper chHelper;
 
-        devHelperVPPM.CreateTransmitter("THE_TRANSMITTER1");
-		/****************************************TX-SIGNAL************************************
-		 * 1000-SIGNAL SIZE, 0.5-DUTY RATIO, 0-BIAS, 9.25E-5-PEAK VOLTAGE, 0-MINIMUM VOLTAGE
-		 ************************************************************************************/
-		devHelperVPPM.SetTXSignal("THE_TRANSMITTER1", 1000, 0.5, 0, 9.25e-5, 0);
-		devHelperVPPM.SetTrasmitterParameter("THE_TRANSMITTER1", "Bias", 0);//SETTING BIAS VOLTAGE MOVES THE SIGNAL VALUES BY THE AMOUNT OF BIASING
-		devHelperVPPM.SetTrasmitterParameter("THE_TRANSMITTER1", "SemiAngle", 35);
-		devHelperVPPM.SetTrasmitterParameter("THE_TRANSMITTER1", "Azimuth", 0);
-		devHelperVPPM.SetTrasmitterParameter("THE_TRANSMITTER1", "Elevation",180.0);
-		devHelperVPPM.SetTrasmitterPosition("THE_TRANSMITTER1", 0.0, 0.0, 52.0);
-		devHelperVPPM.SetTrasmitterParameter("THE_TRANSMITTER1", "Gain", 70);
-		devHelperVPPM.SetTrasmitterParameter("THE_TRANSMITTER1", "DataRateInMBPS",0.3);
+        NodeContainer UEcluster[g_AP_number];
+  		NetDeviceContainer dev[g_AP_number]; // all dev (tx & rx) in APn's cluster
+        /*--- setup transmitter, receiver, channel for every paired AP & UE ---*/
+		for(node* nAP : node::transmitter){
+            for(int UEid : nAP->get_connected()){
+                UEcluster[nAP->id].Add(allUE.Get(UEid)); // add this UE into AP's cluster container
 
+                std::string transmitterName = makeName("TRANSMITTER", nAP->id, UEid); // device name: "TRANSMITTER-<APid>-<UEid>"
+                devHelperVPPM.CreateTransmitter(transmitterName);
+                /****************************************TX-SIGNAL************************************
+                 * 1000-SIGNAL SIZE, 0.5-DUTY RATIO, 0-BIAS, 9.25E-5-PEAK VOLTAGE, 0-MINIMUM VOLTAGE
+                 ************************************************************************************/
+                devHelperVPPM.SetTXSignal(transmitterName, 1000, 0.5, 0, 9.25e-5, 0);
+                devHelperVPPM.SetTrasmitterParameter(transmitterName, "Bias", 0);//SETTING BIAS VOLTAGE MOVES THE SIGNAL VALUES BY THE AMOUNT OF BIASING
+                devHelperVPPM.SetTrasmitterParameter(transmitterName, "SemiAngle", 35);
+                devHelperVPPM.SetTrasmitterParameter(transmitterName, "Azimuth", 0);
+                devHelperVPPM.SetTrasmitterParameter(transmitterName, "Elevation",180.0);
+                devHelperVPPM.SetTrasmitterPosition(transmitterName, 0.0, 0.0, 0.0);
+                devHelperVPPM.SetTrasmitterParameter(transmitterName, "Gain", 70);
+                devHelperVPPM.SetTrasmitterParameter(transmitterName, "DataRateInMBPS",0.3);
+                //Creating and setting properties for the first receiver.
+                std::string receiverName = makeName("RECEIVER", nAP->id, UEid);
+                devHelperVPPM.CreateReceiver(receiverName);
+                devHelperVPPM.SetReceiverParameter(receiverName, "FilterGain", 1);
+                devHelperVPPM.SetReceiverParameter(receiverName, "RefractiveIndex", 1.5);
+                devHelperVPPM.SetReceiverParameter(receiverName, "FOVAngle", 100); // original example is 28.5
+                devHelperVPPM.SetReceiverParameter(receiverName, "ConcentrationGain", 0);
+                devHelperVPPM.SetReceiverParameter(receiverName, "PhotoDetectorArea",1.3e-5);
+                devHelperVPPM.SetReceiverParameter(receiverName, "RXGain", 0);
+                devHelperVPPM.SetReceiverParameter(receiverName, "PhotoDetectorArea",PhotoDetectorArea);
+                devHelperVPPM.SetReceiverParameter(receiverName, "Beta", 1);
+                devHelperVPPM.SetReceiverPosition(receiverName, 0.0, 0.0, 0.0);
+                /****************************************MODULATION SCHEME SETTINGS******************
+                *AVILABLE MODULATION SCHEMES ARE: [1]. VlcErrorModel::PAM4, [2]. VlcErrorModel::OOK [3]. VlcErrorModel::VPPM
+                *AVILABLE MODULATION SCHEMES ARE: [4]. VlcErrorModel::PSK4 [5]. VlcErrorModel::PSK16. [6]. VlcErrorModel::QAM4 [7]. VlcErrorModel::QAM16.
+                ************************************************************************************/
+                devHelperVPPM.SetReceiverParameter(receiverName, "SetModulationScheme",VlcErrorModel::QAM4);
+                //devHelperVPPM.SetReceiverParameter("THE_RECEIVER1", "DutyCycle", 0.85); //Dutycycle is only valid for VPPM
+
+                std::string channelName = makeName("CHANNEL",nAP->id, UEid);
+                chHelper.CreateChannel(channelName);
+                chHelper.SetPropagationLoss(channelName, "VlcPropagationLoss");
+                chHelper.SetPropagationDelay(channelName, 2);
+                chHelper.AttachTransmitter(channelName, transmitterName,&devHelperVPPM);
+                chHelper.AttachReceiver(channelName, receiverName, &devHelperVPPM);
+                chHelper.SetChannelParameter(channelName, "TEMP", 295);
+                chHelper.SetChannelParameter(channelName, "BAND_FACTOR_NOISE_SIGNAL",Band_factor_Noise_Signal );
+                chHelper.SetChannelWavelength(channelName, 380, 780);
+                chHelper.SetChannelParameter(channelName, "ElectricNoiseBandWidth",3 * 1e5);
+                dev[nAP->id].Add(chHelper.Install(allAP.Get(nAP->id), allUE.Get(UEid),
+                                &devHelperVPPM, &chHelper, transmitterName, receiverName, channelName)) ;
+            }
+		}
+
+        // Now add ip/tcp stack to all nodes.
+        InternetStackHelper internet;
+        internet.InstallAll();
+
+        // add IP addresses
+        Ipv4AddressHelper ipv4;
+        Ipv4InterfaceContainer ipInterfs;
+        ApplicationContainer apps;
+        for(int APn=0; APn<g_AP_number; APn++){
+            if (dev[APn].GetN()==0) continue;
+            std::string addressStr = "192.168." + std::to_string(APn) + ".0"; // base addr: 192.168.<APid>.0
+            ipv4.SetBase(addressStr.c_str(),"255.255.255.0");
+            ipInterfs = ipv4.Assign(dev[APn]);
+
+            // Create a source to send packets from n0.  Instead of a full Application
+            // and the helper APIs you might see in other example files, this example
+            // will use sockets directly and register some socket callbacks as a sending
+            // "Application".
+            // Create and bind the socket...
+//            Ptr < Socket > localSocket = Socket::CreateSocket(allAP.Get(APn), TcpSocketFactory::GetTypeId());
+//            Socket::Bind
+//            localSocket->Bind();
+
+//            LogComponentEnable("TcpSocketBase",LOG_FUNCTION);
+            /*--- setup sink ---*/
+            uint16_t servPort = 4000;
+            PacketSinkHelper sink("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(),servPort));
+            apps.Add(sink.Install(UEcluster[APn]));
+
+            /*--- setup socket (to send data???) ---*/
+            for(int i = 1; i<ipInterfs.GetN(); i+=2){
+                std::cout<<"Schedule AP "<<APn<<" ("<<ipInterfs.GetAddress(i-1)<<") with addr "<<ipInterfs.GetAddress(i)<<'\n';
+                Ptr < Socket > localSocket = Socket::CreateSocket(allAP.Get(APn), TcpSocketFactory::GetTypeId());
+                localSocket->Bind();
+                localSocket->BindToNetDevice(dev[APn].Get(i-1));
+                // ...and schedule the sending "Application"; This is similar to what an
+                // ns3::Application subclass would do internally.
+                Simulator::ScheduleNow(&StartFlow, localSocket, ipInterfs.GetAddress(i), servPort);
+            }
+//            LogComponentDisable("TcpSocketBase",LOG_FUNCTION);
+        }
+
+        // setup ip routing table
+        Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+        // setup packet sink
+//        uint16_t servPort = 4000;
+//        PacketSinkHelper sink("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), servPort));
+//        ApplicationContainer apps = sink.Install(allUE);
+
+        apps.Start(Seconds(0.0));
+        apps.Stop(Seconds(4.0));
+
+        /*--- set up callback trace ---*/
+        for(node* nAP : node::transmitter){
+            for(int iUE : nAP->get_connected()){
+                std::string receiverName = makeName("RECEIVER",nAP->id,iUE);
+                Ptr<VlcRxNetDevice> rxPtr = devHelperVPPM.GetReceiver(receiverName);
+                rxPtr->TraceConnectWithoutContext("PhyRxEnd", MakeCallback(&RxEnd)); //traces to allow us to see what and when data is sent through the network
+            }
+        }
+
+        //Ask for ASCII and pcap traces of network traffic
+        AsciiTraceHelper ascii;
+
+        chHelper.EnableAsciiAll (ascii.CreateFileStream ("./log/tcp-large-transfer.tr"));
+        chHelper.EnablePcapAll ("./log/tcp-large-transfer"); // call VlcChannelHelper::EnablePcapAll
 
 		// netAnim is shitty as FUCK i'd rather use gnuplot if i dont need animation. or maybe im just dumb.
-		AnimationInterface anim("../dat/visible-light-communication.xml");
+		//AnimationInterface anim("../dat/visible-light-communication.xml");
 
 		Simulator::Stop(Seconds(5.0));
 		Simulator::Run();
+
+        double throughput = ((Received.back() * 8)) / theTime.back(); // bps
+        std::cout << "throughput value is " << throughput << std::endl;
+
         Simulator::Destroy();
 
         node::UE_number=80;
@@ -230,8 +310,10 @@ int main(int argc, char *argv[]) {
 //begin implementation of sending "Application"
 void StartFlow(Ptr<Socket> localSocket, Ipv4Address servAddress,
 		uint16_t servPort) {
+	NS_LOG_DEBUG("AP "<<localSocket->GetNode()->GetId()<<" Start flow to "<<servAddress<<'\n');
+
 	//NS_LOG_UNCOND("helooooooooooooooooo StartFlow");
-	localSocket->Connect(InetSocketAddress(servAddress, servPort)); //connect
+	if (localSocket->Connect(InetSocketAddress(servAddress, servPort)) == -1) std::cout<<"Failed to connect!\n"; //connect
 
 	// tell the tcp implementation to call WriteUntilBufferFull again
 	// if we blocked and new tx buffer space becomes available
@@ -241,6 +323,8 @@ void StartFlow(Ptr<Socket> localSocket, Ipv4Address servAddress,
 
 void WriteUntilBufferFull(Ptr<Socket> localSocket, uint32_t txSpace) {
 	//NS_LOG_UNCOND("helooooooooooooooooo WriteUntilBufferFull");
+	NS_LOG_DEBUG("AP "<<localSocket->GetNode()->GetId()<<" writing... ");
+
 	while (currentTxBytes < totalTxBytes && localSocket->GetTxAvailable() > 0) {
 
 		uint32_t left = totalTxBytes - currentTxBytes;
@@ -250,20 +334,23 @@ void WriteUntilBufferFull(Ptr<Socket> localSocket, uint32_t txSpace) {
 		toWrite = std::min (toWrite, localSocket->GetTxAvailable ());
 
 		Ptr<Packet> p = Create<Packet>(&data[dataOffset], toWrite);
-		Ptr<Node> startingNode = localSocket->GetNode();
-		Ptr<VlcTxNetDevice> txOne = DynamicCast<VlcTxNetDevice>(startingNode->GetDevice(0) );
+//		Ptr<Node> startingNode = localSocket->GetNode();
+//		Ptr<VlcTxNetDevice> txOne = DynamicCast<VlcTxNetDevice>(startingNode->GetDevice(0) );
+        Ptr<VlcTxNetDevice> txOne = DynamicCast<VlcTxNetDevice>(localSocket->GetBoundNetDevice());
 		txOne->EnqueueDataPacket(p);
 
 		int amountSent = localSocket->Send (&data[dataOffset], toWrite, 0);
 		if(amountSent < 0)
 		{
 			// we will be called again when new tx space becomes available.
+			NS_LOG_DEBUG(" buffer full. Wait. \n");
 			return;
 		}
-
+        NS_LOG_DEBUG(amountSent<<" bytes... ");
 		currentTxBytes += amountSent;
 	}
-
+	NS_LOG_DEBUG("complete. \n");
+    currentTxBytes = 0;
 	localSocket->Close();
 }
 /*
