42a72be46ee2294832f1beac34f179cca0ca07bb
alex-ubuntu <rainbowpulp@gmail.com>
2020-10-30 11:28:57 +0800

    improve wafshellRun.sh ability to read cmd arg = more automization

--- wafshellRun.sh
+++ wafshellRun.sh
@@ -6,7 +6,7 @@ writeToCsvAndConsole(){
 	outputText=$1 #argument 1: text to output to ALL the files
 	prefix=$2 #argument 2: file prefix
 	#echo "$outputText"
-	for file in 'goodput' 'time' 'shannon' 'dropRate' 'aveConnNum' 'power' 'dropTrigCnt' 'oldBreakTrigCnt' 'blockedBandwidth'
+	for file in 'goodput' 'time' 'shannon' 'dropRate' 'power' 'dropTrigCnt' 'oldBreakTrigCnt' 'blockedBandwidth' 'totalReqBandwidth' 'pruneCnt'
 	do
 		#echo "./log/${prefix}_${file}.csv"
 		printf "$outputText" >> ./log/${prefix}_${file}.csv
@@ -19,25 +19,34 @@ echo "Remeber to set RBmode, TDMAmode, RAmode..."
 echo ""
 
 logfile="./log/automate.log"
-varname="paramX"
-RBmode="GC"
-TDMAmode="GA"
-RAmode="BP"
+varname="UE_number"
+if (( $# < 3 )); then
+# default
+	echo "No cmd args / Not enough args. Default mode GC-GA-BP."
+	RBmode="GC"
+	TDMAmode="GA"
+	RAmode="BP"
+else
+# read modes from cmd arg
+	RBmode=$1; shift
+	TDMAmode=$1; shift
+	RAmode=$1; shift
+fi
+
 prefix="${varname}-${RBmode}-${TDMAmode}-${RAmode}"
 echo "Variable: $varname ..." | tee $logfile 
 echo "Output file prefix: ${prefix}"
 for filename in ./log/${prefix}_*.csv; do
-	rm -- "$filename"
-	echo "Removed ${filename}"
+	rm --verbose -- "$filename"
 done
 
-for var in {0..3..1} # paramX from 1 to 3 step 1
+for var in {10..70..10} # UE_number from 10 to 70 step 10
 do
 	# (use tee so that it shows up in console AND log)
 	echo "-------------------------$varname = $var-----------------------------" | tee -a $logfile
 	#printf "$var" | tee -a ./log/${prefix}_goodput.csv ./log/${prefix}_time.csv ./log/${prefix}_shannon.csv ./log/${prefix}_dropRate.csv ./log/${prefix}_aveConnNum.csv ./log/${prefix}_power.csv ./log/${prefix}_dropTrigCnt.csv ./log/${prefix}_oldBreakTrigCnt.csv > /dev/null
 	writeToCsvAndConsole "$var" ${prefix}
-	for ep in {1..80..2} # repeat simulation 40 times
+	for ep in {1..40..2} # repeat simulation 40 times
 	do
 		# execute two simulation at once:
 		echo "epoch $ep" ; ./build/release/scratch/thesis/thesis --Var_name=$varname --$varname=$var --RB_mode=$RBmode --TDMA_mode=$TDMAmode --RA_mode=$RAmode>> $logfile & \
@@ -47,5 +56,9 @@ do
 	#printf "\n" | tee -a ./log/${prefix}_goodput.csv ./log/${prefix}_time.csv ./log/${prefix}_shannon.csv ./log/${prefix}_dropRate.csv ./log/${prefix}_aveConnNum.csv ./log/${prefix}_power.csv ./log/${prefix}_dropTrigCnt.csv ./log/${prefix}_oldBreakTrigCnt.csv > /dev/null
 	writeToCsvAndConsole "\n" ${prefix}
 done
-echo "!!!!!!!!!LAMBDA IS 3, new breakpoint & drop trigger ..DON'T OVERWRITE!!!!!!!"
 
+if [ ! -d log/$varname ]; then
+	mkdir log/$varname
+fi
+mv log/*.csv log/$varname/
+echo "moved to log/$varname/"
